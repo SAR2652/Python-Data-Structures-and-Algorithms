2aec770ef19c52094e62e651b62aaa33e79de3ee
Sarvesh Relekar <sar265278@gmail.com>
2020-04-20 04:21:15 +0530

    Generated Script to update README.MD

---
+++ update_readme.py
@@ -0,0 +1,29 @@
+import os, subprocess
+
+f = open('README.MD')
+count = 0
+intro = ''
+while count < 7:
+    intro += f.readline()
+    count += 1
+
+f.close()
+
+f = open('sample.txt', 'w')
+process = subprocess.Popen(['tree'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+stdout, stderr = process.communicate()
+f.write(stdout.decode('utf-8'))
+f.close()
+
+op = open('README.MD', 'w')
+f = open('sample.txt')
+op.write(intro)
+for line in f.readlines():
+    op.write(line + '<br>' + '\n')
+
+op.close()
+
+os.remove('sample.txt')
+
+
+
