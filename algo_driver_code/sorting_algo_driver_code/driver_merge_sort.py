c434b7e5d9887354c1b13e9ee0811298f6c565e5
SAR2652 <sar265278@gmail.com>
2020-10-05 10:08:56 +0530

    Implemented Merge Sort

--- algo_driver_code/sorting_algo_driver_code/driver_merge_sort.py
+++ algo_driver_code/sorting_algo_driver_code/driver_merge_sort.py
@@ -2,6 +2,6 @@ from Algorithms.sorting_algorithms import mergeSort
 
 arr = [int(x) for x in input("Enter an array of integers : ").strip().split(' ')]
 n = len(arr)
-arr = mergeSort(arr,0,n-1)
+mergeSort(arr,0,n-1)
 
 print("Sorted Array : {}".format(arr))
