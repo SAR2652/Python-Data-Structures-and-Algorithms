efdf165eead03dbaf67090c0e0fd176357079ed0
teanth98 <teanth98@gmail.com>
2020-10-05 13:07:20 +0530

    Quick sort

--- Algorithms/sorting_algorithms.py
+++ Algorithms/sorting_algorithms.py
@@ -6,6 +6,7 @@ def BubbleSort(arr):
 
     return arr
 
+
 def InsertionSort(arr):
     for i in range(len(arr)):
         temp = arr[i]
@@ -27,3 +28,23 @@ def SelectionSort(a):
                 min = j
         a[i], a[min] = a[min], a[i]
     return a
+
+
+def partition(arr, low, high):
+    i = (low-1)
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
