631980d0648fb8a5342ab9c0f69e31f62786c744
teanth98 <teanth98@gmail.com>
2020-10-05 13:47:07 +0530

    heapsort implementation

--- Algorithms/sorting_algorithms.py
+++ Algorithms/sorting_algorithms.py
@@ -48,3 +48,36 @@ def quickSort(arr, low, high):
         pi = partition(arr, low, high)
         quickSort(arr, low, pi-1)
         quickSort(arr, pi+1, high)
+
+def heapify(arr, n, i): 
+    largest = i   
+    l = 2 * i + 1      
+    r = 2 * i + 2      
+  
+
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i]  
+  
+
+        heapify(arr, n, largest) 
+  
+
+def heapSort(arr): 
+    n = len(arr) 
+  
+
+    for i in range(n // 2 - 1, -1, -1): 
+        heapify(arr, n, i) 
+  
+
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]    
+        heapify(arr, i, 0)
